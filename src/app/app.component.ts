2f30ed43e78457b1608f6e02ae7fb955c8ce5bdc
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-02-22 14:46:55 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  public toggleSideBar: boolean = true;
+export class AppComponent {
 
   constructor(
     private translate: TranslateService
   ) {
     this.translate.setDefaultLang('en');
     this.translate.use(translate.getBrowserLang() || 'en');
-  }
-
-  ngOnInit(): void { }
-
-  public onSideBarToggle(toggleSideBar: boolean): void {
-    this.toggleSideBar = toggleSideBar;
   }
 
 }
